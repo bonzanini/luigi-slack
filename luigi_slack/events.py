193981c4305a86e4f937b9df88b4d615d2045211
Marco Bonzanini <marco.bonzanini@gmail.com>
2015-11-09 11:44:55 +0000

    More event types, and label formatting

--- luigi_slack/events.py
+++ luigi_slack/events.py
@@ -1,3 +1,12 @@
 SUCCESS = "SUCCESS"
 FAILURE = "FAILURE"
+MISSING = "MISSING"
 START = "START"
+PROCESSING_TIME = "PROCESSING_TIME"
+
+def event_label(event):
+    """Format event constants for Slack
+
+    e.g. from `MISSING` to `*Missing*`
+    """
+    return "*{}*".format(event.lower().title())
