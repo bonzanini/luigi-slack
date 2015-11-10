17a2a280bcafd7b9f31facb3013205866dc0077a
Marco Bonzanini <marco.bonzanini@gmail.com>
2015-11-10 08:03:39 +0000

    Added SlackBotConf

--- luigi_slack/__init__.py
+++ luigi_slack/__init__.py
@@ -1,4 +1,5 @@
 from pkg_resources import get_distribution
 from luigi_slack.api import SlackBot, notify
+from luigi_slack.slack_api import SlackBotConf
 
 __version__ = get_distribution('luigi_slack').version
