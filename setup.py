27edddc9d5ffc23e58019186b3f36d341d946744
Marco Bonzanini <marco.bonzanini@gmail.com>
2015-11-11 07:23:02 +0000

    Added dependencies

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ long_description = ''
 
 setup(
     name='luigi-slack',
-    version='0.1.1',
+    version='0.1.2',
     description='Send Slack notifications to report on Luigi pipelines',
     long_description=long_description,
     author='Marco Bonzanini',
@@ -12,12 +12,20 @@ setup(
     url='https://github.com/bonzanini',
     license='MIT',
     packages=find_packages(),
+    install_requires=[
+        'luigi>=1.3',
+        'slackclient>=0.16'
+    ],
     include_package_data=True,
     zip_safe=False,
     classifiers=[
         'Environment :: Console',
         'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: System :: Monitoring',
     ]
 )
